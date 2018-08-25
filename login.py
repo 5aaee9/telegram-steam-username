577d9c335a06521fe2b84cb906c8c6fc45712378
Indexyz <jiduye@gmail.com>
2018-08-25 17:34:00 +0800

    ref: add requirement.txt

--- login.py
+++ login.py
@@ -5,3 +5,6 @@ from src.config import get_config
 async def main():
     bot_client = await bot.make_client()
     await bot.login_client(bot_client)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
